4bcdb7df5dbf7db3a7a0ca6a0b80d7c69c6ab2d0
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-04-22 21:03:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_ID = "10247139"
 
 API_HASH = "96b46175824223a33737657ab943fd6a"
 
-BOT_TOKEN = "5706217182:AAELnRWjsHatV-yJ5MvNgb7yAz2-QF6EWf8"
+BOT_TOKEN = "5607218709:AAERZsV38zZgH_F9uFZbnXs3ljnppZUlGvA"
 
 MONGO_DB_URI = "mongodb+srv://ultra:sonic@cluster0.pe1i6eu.mongodb.net/?retryWrites=true&w=majority"
 INDEX_ID = -1001884130912
